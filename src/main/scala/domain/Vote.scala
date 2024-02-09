bbb46dd1b057cc5fb9100027e0c1d97867d9570c
Thomas Legoahec <thomas.legoahec@ubisoft.com>
2024-02-09 11:43:29 +0100

    feat: add name and push march results

--- src/main/scala/domain/Vote.scala
+++ src/main/scala/domain/Vote.scala
@@ -1,7 +1,7 @@
 package domain
 
 
-case class Vote(vote1: String, vote2: String, vote3: String, vote4: String)
+case class Vote(vote1: String, vote2: String, vote3: String, vote4: String, name: String)
 
 case class VoteResult(counterVote: Int =0 , nbVote1: Int =0, nbVote2: Int=0, nbVote3: Int=0, nbVote4: Int=0)
 
